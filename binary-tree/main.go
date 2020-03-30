8a9ca447fbb9ab4a1c1b872dc41f6f73d5e542cc
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-03-29 21:48:06 -0300

    Teste no método RotateRight

--- binary-tree/main.go
+++ binary-tree/main.go
@@ -9,27 +9,17 @@ func main() {
 
 	tr := new(tree.Tree)
 
-	tr.Add(9)
-	tr.Add(15)
-	tr.Add(18)
-	tr.Add(12)
-	tr.Add(13)
-	tr.Add(10)
-	tr.Add(11)
-	tr.Add(5)
-	tr.Add(7)
-	tr.Add(6)
-	tr.Add(8)
-	tr.Add(2)
-	tr.Add(1)
-	tr.Add(4)
+	tr.Add(5)	
+	tr.Add(7)	
+	tr.Add(3)	
+	tr.Add(4)	
+	tr.Add(2)	
 
 	fmt.Println("\nÁrvore:")
 	tr.PrintInOrder()
 
 	fmt.Println("----------------------------------------")
-	tr.Remove(12)
+	tr.RotateRight()
 
-	//fmt.Println("\n\nÁrvore:")
 	tr.PrintInOrder()
 }
